a2e1c782af2b161483a57d1878214a46f4487296
AnonDogesh <DivineLeaf@proton.me>
2026-03-03 19:51:18 +0530

    Add missing LinearProgressIndicator import in HomeScreen

--- app/src/main/java/com/example/motes/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/motes/ui/home/HomeScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
